0191a6170f8300ecb982bae903c8ae8e830b9523
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-06-27 06:33:12 -0400

    Current tests passing; will need more

--- database/database_test.go
+++ database/database_test.go
@@ -8,7 +8,7 @@ func TestCreateNewBoardDB(t *testing.T) {
 	OpenTestDBConnection()
 
 	// TODO: Write test cases based on results from DB
-	result := CreateNewBoardDB("json-parser", true)
+	result := CreateNewBoardDB("dev-tui", true)
 	if !result {
 		t.Fail()
 		t.Error("Board not stored in DB")
